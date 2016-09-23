e589c02855ba8ebb7611e4b033e9df2783c08c23
POEMisaMisa <eon40qb8mk@gmail.com>
2016-09-23 19:59:53 +0700

    Changed local paths

--- build_exe.bat
+++ build_exe.bat
@@ -1,2 +1,2 @@
-"D:\Program Files\AutoIt3\aut2exe\aut2exe.exe" /in "D:\Delphi\Tools\POE Build grabber\src\AtlasOfBuild.au3" /icon "D:\Delphi\Tools\POE Build grabber\icon.ico"
+"D:\Program Files\AutoIt3\aut2exe\aut2exe.exe" /in "E:\Projects\POEMisaMisa\POE-Atlas-of-Build\src\AtlasOfBuild.au3" /icon "E:\Projects\POEMisaMisa\POE-Atlas-of-Build\icon.ico"
 pause
